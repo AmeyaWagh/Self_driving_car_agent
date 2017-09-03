85318df8c0648067f6d24c7dc71e9496b093016c
AmeyaWagh <ameya555@gmail.com>
2017-09-03 16:09:25 -0400

    single traffic car model created

--- new_sim.py
+++ new_sim.py
@@ -12,24 +12,52 @@ class roadModel():
         self.road = np.zeros(
             (self.road_length, self.lane), dtype='float')
 
-    def updateRoad(self,car):
-        self.road[car.x,car.y] = car.speed
+    def updateRoad(self,agentCar,trafficCarsList):
+        self.road[:,:]=0
+        self.road[agentCar.x,agentCar.y] = agentCar.speed
+        for everyCar in trafficCarsList:
+            self.road[everyCar.x,everyCar.y] = everyCar.speed
+
 
     def displayRoad(self):
+        os.system('clear')
         print '-'*80
         print self.road    
         print '-'*80
 
-class car():
-    def __init__(self,newRoad):
+class agent_car():
+    def __init__(self,roadInstance):
+        self.road = roadInstance
         self.x = 10     
-        self.y = np.random.randint(0,newRoad.lane)
+        self.y = np.random.randint(0,self.road.lane)
         self.speed = 50.0
 
+class traffic_car():
+    def __init__(self,roadInstance):
+        self.road = roadInstance
+        self.x = roadInstance.road_length-1     
+        self.y = np.random.randint(0,self.road.lane)
+        self.speed = 50.0
+
+    def iterate(self): 
+        if( self.x > 0 ):
+            self.x -= 1
+        else:
+            self.speed = 0
+
+
 if __name__ == '__main__':
-   newRoad = roadModel()
-   newRoad.displayRoad()
-   agent_car = car(newRoad)
-   newRoad.updateRoad(agent_car)             
-   newRoad.displayRoad()
-                    
+    newRoad = roadModel()
+    # newRoad.displayRoad()
+    agentCar = agent_car(newRoad)
+    trafficCar = traffic_car(newRoad) 
+    iteration=0
+    
+    while(1):
+        time.sleep(0.5)
+        newRoad.updateRoad(agentCar,[trafficCar])             
+        # newRoad.updateRoad(trafficCar)
+        trafficCar.iterate()             
+        newRoad.displayRoad()
+        print "iteration",iteration
+        iteration+=1            
